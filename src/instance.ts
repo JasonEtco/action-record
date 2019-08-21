eca3dff26f0bfb196c4ca6935b79390b81f75531
Jason Etcovitch <jasonetco@github.com>
2019-08-21 01:38:33 -0400

    Implement basic findOne/All methods

---
+++ src/instance.ts
@@ -0,0 +1,11 @@
+export default class Instance {
+  public issue: any
+
+  constructor (issue: any) {
+    this.issue = issue
+  }
+
+  async destroy () {
+    // TODO: Write code
+  }
+}
--- src/model.ts
+++ src/model.ts
@@ -6,6 +6,7 @@ import github from '@actions/github'
 import colorHash from 'color-hash'
 import octokit from './octokit'
 import ActionRecord from './action-record'
+import Instance from './instance'
 
 export async function createModelLabel (name: string) {
   // Create new label if it doesn't exist
@@ -29,16 +30,45 @@ export async function registerModels (actionRecord: ActionRecord) {
   return Promise.all(modelFiles.map(async modelFile => {
     const model = require(path.join(modelsDir, modelFile))
     await createModelLabel(model.name)
-    actionRecord.models[model.name] = new Model(model.schema)
+    actionRecord.models[model.name] = new Model(model.name, model.schema)
   }))
 }
 
 export class Model {
+  public name: string
   public schema: Joi
   public validate: Joi.validate
 
-  constructor (schema: Joi) {
+  constructor (name: string, schema: Joi) {
+    this.name = name
     this.schema = schema
     this.validate = schema.validate
+  }
+
+  whereToStr (where: any) {
+    const jsonStr = JSON.stringify(where, null, 2)
+    return jsonStr.slice(1, jsonStr.length - 2)
+  }
+
+  private async searchForIssues () {
+    // Search for issues by this label
+    const issues = await octokit.search.issuesAndPullRequests({
+      q: `is:issue label:${this.name}`
+    })
+    return issues.data.items
+  }
+
+  async findOne (where: any) {
+    const issues = await this.searchForIssues()
+    const whereStr = this.whereToStr(where)
+    const found = issues.find(issue => (issue.body as string).includes(whereStr))
+    return new Instance(found)
+  }
+
+  async findAll (where: any): Promise<Instance[]> {
+    const issues = await this.searchForIssues()
+    const whereStr = this.whereToStr(where)
+    const found = issues.filter(issue => (issue.body as string).includes(whereStr))
+    return found.map(item => new Instance(item))
   }
 }
