0697c5fd4453e180757571962edf9c0d4a3e0421
Jason Etcovitch <jasonetco@github.com>
2019-08-21 01:53:12 -0400

    More comments

--- src/model.ts
+++ src/model.ts
@@ -15,11 +15,17 @@ export default class Model {
     this.validate = schema.validate
   }
 
-  whereToStr (where: any) {
+  /**
+   * Convert a `where` object to a string for proper searching.
+   */
+  private whereToStr (where: any) {
     const jsonStr = JSON.stringify(where, null, 2)
     return jsonStr.slice(1, jsonStr.length - 2)
   }
 
+  /**
+   * Call the search API to return all issues with this model's label.
+   */
   private async searchForIssues () {
     // Search for issues by this label
     const issues = await octokit.search.issuesAndPullRequests({
@@ -28,6 +34,9 @@ export default class Model {
     return issues.data.items
   }
 
+  /**
+   * Find one record that matches the provided filter object.
+   */
   async findOne (where: any) {
     const issues = await this.searchForIssues()
     const whereStr = this.whereToStr(where)
@@ -35,13 +44,23 @@ export default class Model {
     return new Instance(found)
   }
 
-  async findAll (where: any): Promise<Instance[]> {
+  /**
+   * Find all records that match the provided filter object.
+   */
+  async findAll (where?: any): Promise<Instance[]> {
     const issues = await this.searchForIssues()
-    const whereStr = this.whereToStr(where)
-    const found = issues.filter(issue => (issue.body as string).includes(whereStr))
-    return found.map(item => new Instance(item))
+    if (where) {
+      const whereStr = this.whereToStr(where)
+      const found = issues.filter(issue => (issue.body as string).includes(whereStr))
+      return found.map(item => new Instance(item))
+    } else {
+      return issues.map(item => new Instance(item))
+    }
   }
 
+  /**
+   * Create a new record
+   */
   async create (opts: any): Promise<Instance> {
     // Validate the provided object against the model's schema
     await this.schema.validate(opts)
