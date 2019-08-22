74a4a565256b1bed558e0ed84544b13b1219eb53
Jason Etcovitch <jasonetco@github.com>
2019-08-22 02:57:07 -0400

    Improve instance types

--- src/instance.ts
+++ src/instance.ts
@@ -1,8 +1,15 @@
-export default class Instance {
-  public issue: any
+interface IssueRecord {
+  action_record_id: number
+  created_at: string
+  issue_number: number
+  [key: string]: any
+}
 
-  constructor (issue: any) {
-    this.issue = issue
+export default class Instance {
+  public issue_record: IssueRecord
+
+  constructor (issue_record: IssueRecord) {
+    this.issue_record = issue_record
   }
 
   async destroy () {
