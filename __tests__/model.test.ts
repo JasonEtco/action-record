26970f038f6004ab85a8e7daf2090bf568cdc8d7
Jason Etcovitch <jasonetco@github.com>
2019-08-22 22:06:31 -0400

    Use a real uuid

--- __tests__/model.test.ts
+++ __tests__/model.test.ts
@@ -20,7 +20,12 @@ describe('Model', () => {
           number: 1,
           labels: [{ name: model.name }],
           created_at: '2009-07-12T20:10:41Z',
-          body: '```\n{\n  "action_record_id": "some-string",\n  "login": "JasonEtco"\n}\n```'
+          body: '```\n{\n  "action_record_id": "29da0a67-062a-46f8-b397-7eac68513492",\n  "login": "JasonEtco"\n}\n```'
+        }, {
+          number: 1,
+          labels: [{ name: model.name }],
+          created_at: '2009-07-12T20:10:41Z',
+          body: '```\n{\n  "action_record_id": "66eb86bd-046b-40b4-9d74-e81c41360ae3",\n  "login": "SomeoneElse"\n}\n```'
         }]
       })
   })
@@ -28,17 +33,13 @@ describe('Model', () => {
   describe('#findOne', () => {
     it('returns the expected record', async () => {
       const record = await model.findOne({ login: 'JasonEtco' }) as Instance
-
       expect(record).toBeInstanceOf(Instance)
       expect(record.login).toBe('JasonEtco')
       expect(record).toMatchSnapshot()
     })
 
     it('returns null if no record was found', async () => {
-      const record = await model.findOne({
-        login: 'nope'
-      })
-
+      const record = await model.findOne({ login: 'nope' })
       expect(record).toBeNull()
     })
   })
