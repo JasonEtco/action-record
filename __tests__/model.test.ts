4ce059a76e6da7830ef2349d7a4759b09184dc6e
Jason Etcovitch <jasonetco@github.com>
2019-08-22 20:59:29 -0400

    Spike out some tests

---
+++ __tests__/model.test.ts
@@ -0,0 +1,14 @@
+import Model from '../src/model'
+
+describe('Model', () => {
+  describe('#findOne', () => {
+    it.todo('returns the expected record')
+    it.todo('returns null if no record was found')
+  })
+
+  describe('#findAll', () => {
+    it.todo('returns the expected records')
+    it.todo('returns the expected records with a `where` object')
+    it.todo('returns null if no records were found')
+  })
+})
