719459a5840ad03cac57e62d512319094c0397b3
Jason Etcovitch <jasonetco@github.com>
2019-08-22 21:47:48 -0400

    Improve Instance types

--- src/instance.ts
+++ src/instance.ts
@@ -5,11 +5,28 @@ export interface IssueRecord {
   [key: string]: any
 }
 
-export default class Instance {
-  public issue_record: IssueRecord
+export default class Instance implements IssueRecord {
+  readonly action_record_id: number
+  readonly created_at: string
+  readonly issue_number: number
 
   constructor (issue_record: IssueRecord) {
-    this.issue_record = issue_record
+    const {
+      action_record_id,
+      created_at,
+      issue_number,
+      ...rest
+    } = issue_record
+
+    this.action_record_id = action_record_id
+    this.created_at = created_at
+    this.issue_number = issue_number
+
+    for (const key in rest) {
+      Object.defineProperty(this, key, {
+        get: () => rest[key]
+      })
+    }
   }
 
   async destroy () {
