781edff02ca3ad7ad2f89ded40d5cc92272dfdd1
Jason Etcovitch <jasonetco@github.com>
2019-08-22 21:47:42 -0400

    More tests

--- __tests__/model.test.ts
+++ __tests__/model.test.ts
@@ -1,9 +1,52 @@
+import Joi from '@hapi/joi'
+import nock from 'nock'
 import Model from '../src/model'
+import Instance from '../src/instance'
+
+interface UserModel {
+  login: string
+}
 
 describe('Model', () => {
+  let model: Model
+
+  beforeEach(() => {
+    model = new Model({
+      name: 'user',
+      schema: Joi.object({
+        login: Joi.string()
+      })
+    })
+
+    nock('https://api.github.com')
+      .get(/^\/search\/issues\?q=/).reply(200, {
+        items: [{
+          number: 1,
+          labels: [{ name: model.name }],
+          created_at: '2009-07-12T20:10:41Z',
+          body: '```\n{\n  "action_id": "some-string",\n  "login": "JasonEtco"\n}\n```'
+        }]
+      })
+  })
+
   describe('#findOne', () => {
-    it.todo('returns the expected record')
-    it.todo('returns null if no record was found')
+    it('returns the expected record', async () => {
+      const record = await model.findOne<UserModel>({
+        login: 'JasonEtco'
+      })
+
+      expect(record).toBeInstanceOf(Instance)
+      expect(record.login).toBe('JasonEtco')
+      expect(record).toMatchSnapshot()
+    })
+
+    it('returns null if no record was found', async () => {
+      const record = await model.findOne({
+        login: 'nope'
+      })
+
+      expect(record).toBeNull()
+    })
   })
 
   describe('#findAll', () => {
